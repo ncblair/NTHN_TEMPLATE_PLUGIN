21c68fe3c1cde0edfcbd355bbb581612a0021b0b
Ncblair <nblair@berkeley.edu>
2025-02-15 21:06:40 -0500

    use raw array for param_to_component and format

--- src/parameters/StateManager.cpp
+++ src/parameters/StateManager.cpp
@@ -3,63 +3,65 @@
 #include "StateManager.h"
 #include "../plugin/PluginProcessor.h"
 #include "../plugin/ProjectInfo.h"
+#include <cassert>
 
-
-StateManager::StateManager(PluginProcessor* proc) : 
-    PRESETS_DIR(juce::File::getSpecialLocation(juce::File::SpecialLocationType::userMusicDirectory).getChildFile(
-        juce::String(JucePlugin_Manufacturer) + "_plugins").getChildFile(JucePlugin_Name).getChildFile("presets")
-    ), 
-    PRESET_EXTENSION("." + juce::String(JucePlugin_Name).toLowerCase())
+StateManager::StateManager(PluginProcessor *proc) : PRESETS_DIR(juce::File::getSpecialLocation(juce::File::SpecialLocationType::userMusicDirectory).getChildFile(juce::String(JucePlugin_Manufacturer) + "_plugins").getChildFile(JucePlugin_Name).getChildFile("presets")),
+                                                    PRESET_EXTENSION("." + juce::String(JucePlugin_Name).toLowerCase())
 {
-    param_to_component.resize(TOTAL_NUMBER_PARAMETERS);
     //==============================================================================
     //-> ADD PARAMS/PROPERTIES
     //==============================================================================
     std::vector<std::unique_ptr<juce::RangedAudioParameter>> params;
     property_tree = juce::ValueTree(PROPERTIES_ID);
 
-    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id) {
-        if (PARAMETER_AUTOMATABLE[p_id]) {
+    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id)
+    {
+        if (PARAMETER_AUTOMATABLE[p_id])
+        {
             params.push_back(
                 std::make_unique<juce::AudioParameterFloat>(
-                    juce::ParameterID{PARAMETER_NAMES[p_id], ProjectInfo::versionNumber},   // parameter ID
-                    PARAMETER_NICKNAMES[p_id],   // parameter name
-                    PARAMETER_RANGES[p_id],  // range
-                    PARAMETER_DEFAULTS[p_id],// default value
-                    "", // parameter label (description?)
+                    juce::ParameterID{PARAMETER_NAMES[p_id], ProjectInfo::versionNumber}, // parameter ID
+                    PARAMETER_NICKNAMES[p_id],                                            // parameter name
+                    PARAMETER_RANGES[p_id],                                               // range
+                    PARAMETER_DEFAULTS[p_id],                                             // default value
+                    "",                                                                   // parameter label (description?)
                     juce::AudioProcessorParameter::Category::genericParameter,
                     [p_id](float value, int maximumStringLength) { // Float to String Precision 2 Digits
                         auto to_string_size = PARAMETER_TO_STRING_ARRS[p_id].size();
                         juce::String res;
-                        if (to_string_size > 0 && (unsigned int) value < to_string_size) {
+                        if (to_string_size > 0 && (unsigned int)value < to_string_size)
+                        {
                             res = PARAMETER_TO_STRING_ARRS[p_id][(unsigned long)(value)];
                         }
-                        else {
+                        else
+                        {
                             std::stringstream ss;
                             ss << std::fixed << std::setprecision(2) << value;
                             res = juce::String(ss.str());
                         }
                         return (res + " " + PARAMETER_SUFFIXES[p_id]).substring(0, maximumStringLength);
                     },
-                    [p_id](juce::String text) {
+                    [p_id](juce::String text)
+                    {
                         text = text.upToFirstOccurrenceOf(" " + PARAMETER_SUFFIXES[p_id], false, true);
                         auto to_string_size = PARAMETER_TO_STRING_ARRS[p_id].size();
-                        if (to_string_size > 0) {
+                        if (to_string_size > 0)
+                        {
                             auto beg = PARAMETER_TO_STRING_ARRS[p_id].begin();
                             auto end = PARAMETER_TO_STRING_ARRS[p_id].end();
                             auto it = std::find(beg, end, text);
-                            if (it == end) {
+                            if (it == end)
+                            {
                                 DBG("ERROR: Could not find text in PARAMETER_TO_STRING_ARRS");
                                 return text.getFloatValue();
                             }
                             return float(it - beg);
                         }
                         return text.getFloatValue(); // Convert Back to Value
-                    }
-                )
-            );
+                    }));
         }
-        else {
+        else
+        {
             property_tree.setProperty(PARAMETER_IDS[p_id], PARAMETER_DEFAULTS[p_id], nullptr);
             property_atomics[PARAMETER_NAMES[p_id]].store(PARAMETER_DEFAULTS[p_id]);
         }
@@ -68,9 +70,11 @@ StateManager::StateManager(PluginProcessor* proc) :
 
     param_tree_ptr.reset(new juce::AudioProcessorValueTreeState(*proc, &undo_manager, PARAMETERS_ID, {params.begin(), params.end()}));
     property_tree.addListener(this);
-    
-    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id) {
-        if (PARAMETER_AUTOMATABLE[p_id]) {
+
+    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id)
+    {
+        if (PARAMETER_AUTOMATABLE[p_id])
+        {
             param_tree_ptr->addParameterListener(PARAMETER_NAMES[p_id], this);
         }
     }
@@ -82,39 +86,48 @@ StateManager::StateManager(PluginProcessor* proc) :
     preset_tree.setProperty(PRESET_NAME_ID, DEFAULT_PRESET, nullptr);
 }
 
-StateManager::~StateManager(){
+StateManager::~StateManager()
+{
     property_tree.removeListener(this);
-    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id) {
-        if (PARAMETER_AUTOMATABLE[p_id]) {
+    for (size_t p_id = 0; p_id < PARAM::TOTAL_NUMBER_PARAMETERS; ++p_id)
+    {
+        if (PARAMETER_AUTOMATABLE[p_id])
+        {
             param_tree_ptr->removeParameterListener(PARAMETER_NAMES[p_id], this);
         }
     }
 }
 
-
-float StateManager::param_value(size_t param_id) {
+float StateManager::param_value(size_t param_id)
+{
     // returns the parameter value of a certain ID in a thread safe way
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         return param_tree_ptr->getRawParameterValue(PARAMETER_NAMES[param_id])->load();
     }
-    else {
+    else
+    {
         return property_atomics[PARAMETER_NAMES[param_id]].load();
     }
 }
 
-juce::AudioProcessorValueTreeState* StateManager::get_param_tree(){
+juce::AudioProcessorValueTreeState *StateManager::get_param_tree()
+{
     return param_tree_ptr.get();
 }
 
-juce::ValueTree StateManager::get_property_tree(){
+juce::ValueTree StateManager::get_property_tree()
+{
     return property_tree;
 }
 
-juce::ValueTree StateManager::get_preset_tree() {
+juce::ValueTree StateManager::get_preset_tree()
+{
     return preset_tree;
 }
 
-juce::ValueTree StateManager::get_state() {
+juce::ValueTree StateManager::get_state()
+{
     state_tree = juce::ValueTree(STATE_ID);
     state_tree.appendChild(param_tree_ptr->copyState(), nullptr);
     state_tree.appendChild(property_tree.createCopy(), nullptr);
@@ -122,38 +135,46 @@ juce::ValueTree StateManager::get_state() {
     return state_tree;
 }
 
-void StateManager::save_preset(juce::String preset_name) {
+void StateManager::save_preset(juce::String preset_name)
+{
     // not undo-able
-    preset_tree.setProperty(PRESET_NAME_ID, preset_name, nullptr); 
+    preset_tree.setProperty(PRESET_NAME_ID, preset_name, nullptr);
     preset_tree.setProperty(PRESET_MODIFIED_ID, false, nullptr);
     auto file = PRESETS_DIR.getChildFile(preset_name).withFileExtension(PRESET_EXTENSION);
-    if (!PRESETS_DIR.exists()) {
+    if (!PRESETS_DIR.exists())
+    {
         // create directory if it doesn't exist
         PRESETS_DIR.createDirectory();
     }
-    if (!file.existsAsFile()) {
+    if (!file.existsAsFile())
+    {
         // create file
         file.create();
     }
     auto plugin_state = get_state();
-    
-    std::unique_ptr<juce::XmlElement> xml (plugin_state.createXml());
+
+    std::unique_ptr<juce::XmlElement> xml(plugin_state.createXml());
     auto temp = juce::File::createTempFile("preset_temp");
     xml->writeTo(temp);
     temp.replaceFileIn(file);
 }
 
-void StateManager::load_preset(juce::String preset_name) {
+void StateManager::load_preset(juce::String preset_name)
+{
     auto file = PRESETS_DIR.getChildFile(preset_name).withFileExtension(PRESET_EXTENSION);
-    if (file.existsAsFile()) {
+    if (file.existsAsFile())
+    {
         std::unique_ptr<juce::XmlElement> xmlState = juce::XmlDocument::parse(file);
         load_from(xmlState.get());
     }
 }
 
-void StateManager::load_from(juce::XmlElement* xml) {
-    if (xml != nullptr){
-        if (xml->hasTagName (STATE_ID)){
+void StateManager::load_from(juce::XmlElement *xml)
+{
+    if (xml != nullptr)
+    {
+        if (xml->hasTagName(STATE_ID))
+        {
             auto new_tree = juce::ValueTree::fromXml(*xml);
             param_tree_ptr->state.copyPropertiesAndChildrenFrom(new_tree.getChildWithName(PARAMETERS_ID), &undo_manager);
             property_tree.copyPropertiesFrom(new_tree.getChildWithName(PROPERTIES_ID), &undo_manager);
@@ -163,104 +184,132 @@ void StateManager::load_from(juce::XmlElement* xml) {
     }
 }
 
-
-void StateManager::set_preset_name(juce::String preset_name) {
+void StateManager::set_preset_name(juce::String preset_name)
+{
     preset_tree.setProperty(PRESET_NAME_ID, preset_name, &undo_manager);
 }
 
-juce::String StateManager::get_preset_name() {
-    if (bool(preset_tree.getProperty(PRESET_MODIFIED_ID))) {
+juce::String StateManager::get_preset_name()
+{
+    if (bool(preset_tree.getProperty(PRESET_MODIFIED_ID)))
+    {
         return preset_tree.getProperty(PRESET_NAME_ID).toString() + "*";
     }
-    else {
+    else
+    {
         return preset_tree.getProperty(PRESET_NAME_ID).toString();
     }
 }
 
-void StateManager::update_preset_modified() {
+void StateManager::update_preset_modified()
+{
     // called from UI thread - updates preset_modified property, if the preset has been modified
-    if (preset_modified.exchange(false)) {
+    if (preset_modified.exchange(false))
+    {
         preset_tree.setProperty(PRESET_MODIFIED_ID, true, nullptr);
     }
 }
 
-bool StateManager::get_parameter_modified(size_t param_id, bool exchange_value) {
+bool StateManager::get_parameter_modified(size_t param_id, bool exchange_value)
+{
     return parameter_modified_flags[PARAMETER_NAMES[param_id]].exchange(exchange_value);
 }
 
-juce::RangedAudioParameter* StateManager::get_parameter(size_t param_id) {
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+juce::RangedAudioParameter *StateManager::get_parameter(size_t param_id)
+{
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         return param_tree_ptr->getParameter(PARAMETER_NAMES[param_id]);
     }
-    else {
+    else
+    {
         jassertfalse;
         DBG("Parameter not automatable: can't get RangedAudioParameter of property");
         return nullptr;
     }
 }
 
-void StateManager::begin_change_gesture(size_t param_id) {
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+void StateManager::begin_change_gesture(size_t param_id)
+{
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         auto parameter = get_parameter(param_id);
         parameter->beginChangeGesture();
     }
 }
 
-void StateManager::end_change_gesture(size_t param_id) {
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+void StateManager::end_change_gesture(size_t param_id)
+{
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         auto parameter = get_parameter(param_id);
         parameter->endChangeGesture();
     }
 }
 
-void StateManager::set_parameter(size_t param_id, float value) {
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+void StateManager::set_parameter(size_t param_id, float value)
+{
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         auto range = PARAMETER_RANGES[param_id];
         auto normalized_value = range.convertTo0to1(range.snapToLegalValue(value));
         set_parameter_normalized(param_id, normalized_value);
     }
-    else {
+    else
+    {
         property_tree.setProperty(PARAMETER_IDS[param_id], value, &undo_manager);
     }
 }
 
-void StateManager::set_parameter_normalized(size_t param_id, float normalized_value) {
-    if (normalized_value > 1.0) normalized_value = 1.0;
-    else if (normalized_value < 0.0) normalized_value = 0.0;
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+void StateManager::set_parameter_normalized(size_t param_id, float normalized_value)
+{
+    if (normalized_value > 1.0)
+        normalized_value = 1.0;
+    else if (normalized_value < 0.0)
+        normalized_value = 0.0;
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         auto parameter = get_parameter(param_id);
         parameter->setValueNotifyingHost(normalized_value);
     }
-    else {
+    else
+    {
         auto unnormalized_value = PARAMETER_RANGES[param_id].convertFrom0to1(normalized_value);
         set_parameter(param_id, unnormalized_value);
     }
 }
 
-void StateManager::randomize_parameter(size_t param_id, float min, float max) {
+void StateManager::randomize_parameter(size_t param_id, float min, float max)
+{
     // min, max between 0 and 1
     jassert(min >= 0.0f && max <= 1.0f && max >= min);
     auto value = rng.nextFloat() * (max - min) + min;
-    if (PARAMETER_AUTOMATABLE[param_id]) {
+    if (PARAMETER_AUTOMATABLE[param_id])
+    {
         auto parameter = get_parameter(param_id);
         parameter->setValueNotifyingHost(value);
     }
-    else {
+    else
+    {
         auto range = PARAMETER_RANGES[param_id];
         auto scaled_val = range.convertFrom0to1(value);
         property_tree.setProperty(PARAMETER_IDS[param_id], scaled_val, &undo_manager);
     }
 }
 
-juce::String StateManager::get_parameter_text(size_t param_id) {
+juce::String StateManager::get_parameter_text(size_t param_id)
+{
     return get_parameter(param_id)->getText(PARAMETER_RANGES[param_id].convertTo0to1(param_value(param_id)), 20);
 }
-void StateManager::reset_parameter(size_t param_id) {
+void StateManager::reset_parameter(size_t param_id)
+{
     set_parameter(param_id, PARAMETER_DEFAULTS[param_id]);
 }
 
-void StateManager::init() {
-    for (size_t i = 0; i < PARAM::TOTAL_NUMBER_PARAMETERS; ++i) {
+void StateManager::init()
+{
+    for (size_t i = 0; i < PARAM::TOTAL_NUMBER_PARAMETERS; ++i)
+    {
         reset_parameter(i);
     }
     // reset value trees
@@ -269,29 +318,36 @@ void StateManager::init() {
     preset_modified.store(false);
 }
 
-void StateManager::randomize_parameters() {
-    for (size_t i = 0; i < PARAM::TOTAL_NUMBER_PARAMETERS; ++i) {
+void StateManager::randomize_parameters()
+{
+    for (size_t i = 0; i < PARAM::TOTAL_NUMBER_PARAMETERS; ++i)
+    {
         randomize_parameter(i);
     }
 }
 
-juce::UndoManager* StateManager::get_undo_manager() {
+juce::UndoManager *StateManager::get_undo_manager()
+{
     return &undo_manager;
 }
 
-void StateManager::valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChanged, const juce::Identifier &property) {
+void StateManager::valueTreePropertyChanged(juce::ValueTree &treeWhosePropertyHasChanged, const juce::Identifier &property)
+{
     // this will be polled by UI to update when UI changes
-    if (treeWhosePropertyHasChanged != preset_tree) {
+    if (treeWhosePropertyHasChanged != preset_tree)
+    {
         preset_modified.store(true);
         any_parameter_changed.store(true);
-        if (treeWhosePropertyHasChanged == property_tree) {
+        if (treeWhosePropertyHasChanged == property_tree)
+        {
             property_atomics[property.toString()].store(float(property_tree.getProperty(property)));
             parameter_modified_flags[property.toString()].store(true);
         }
     }
 }
 
-void StateManager::parameterChanged(const juce::String &parameterID, float newValue) {
+void StateManager::parameterChanged(const juce::String &parameterID, float newValue)
+{
     // parameter changed, note as modified
     // might be called from audio thread, so must be thread safe
     preset_modified.store(true);
@@ -300,15 +356,15 @@ void StateManager::parameterChanged(const juce::String &parameterID, float newVa
     juce::ignoreUnused(newValue);
 }
 
-void StateManager::register_component(size_t id, juce::Component *component) {
-    if (id <= param_to_component.size()) {
-        param_to_component[id].push_back(component);
-    }
+void StateManager::register_component(size_t id, juce::Component *component)
+{
+    assert(id <= TOTAL_NUMBER_PARAMETERS);
+    param_to_component[id].push_back(component);
 }
 
-void StateManager::unregister_component(size_t id, juce::Component* component) {
-    if (id < param_to_component.size()) {
-        auto& vec = param_to_component[id];
-        vec.erase(std::remove(vec.begin(), vec.end(), component), vec.end());
-    }
+void StateManager::unregister_component(size_t id, juce::Component *component)
+{
+    assert(id <= TOTAL_NUMBER_PARAMETERS);
+    std::vector<juce::Component *> &vec = param_to_component[id];
+    vec.erase(std::remove(vec.begin(), vec.end(), component), vec.end());
 }
--- src/plugin/PluginEditor.cpp
+++ src/plugin/PluginEditor.cpp
@@ -5,8 +5,8 @@
 #include "../interface/ParameterSlider.h"
 
 //==============================================================================
-AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (PluginProcessor& p)
-    : AudioProcessorEditor (&p), processorRef (p)
+AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor(PluginProcessor &p)
+    : AudioProcessorEditor(&p), processorRef(p)
 {
     state = processorRef.state.get();
     startTimerHz(int(TIMER_HZ));
@@ -19,10 +19,10 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (PluginProcesso
     addAndMakeVisible(*gain_slider);
 
     // some settings about UI
-    setOpaque (true);
+    setOpaque(true);
     setSize(W, H);
     setColour(0, juce::Colour(0xff00ffa1)); // background color
-    
+
     // resizable window
     setResizable(true, true);
     setResizeLimits((W * 4) / 5, (H * 4) / 5, (W * 3) / 2, (H * 3) / 2);
@@ -34,11 +34,11 @@ AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
     // remove any listeners here
 
     // also, if we have a lookAndFeel object we should call:
-    // setLookAndFeel(nullptr); 
+    // setLookAndFeel(nullptr);
 }
 
 //==============================================================================
-void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
+void AudioPluginAudioProcessorEditor::paint(juce::Graphics &g)
 {
     // Our component is opaque, so we must completely fill the background with a solid colour
     g.fillAll(findColour(0));
@@ -53,20 +53,25 @@ void AudioPluginAudioProcessorEditor::resized()
     gain_slider->setBounds(slider_x, slider_y, slider_size, slider_size);
 }
 
-void AudioPluginAudioProcessorEditor::timerCallback() {
+void AudioPluginAudioProcessorEditor::timerCallback()
+{
     // repaint UI and note that we have updated ui, if parameter values have changed
-    for (size_t i {0}; i < TOTAL_NUMBER_PARAMETERS; ++i) {
-        if (state->get_parameter_modified(i)) {
-            auto& components = state->get_component(i);
-            for (auto& component : components)
+    for (size_t i{0}; i < TOTAL_NUMBER_PARAMETERS; ++i)
+    {
+        if (state->get_parameter_modified(i))
+        {
+            std::vector<juce::Component *> &components = state->get_components(i);
+            for (juce::Component *component : components)
                 component->repaint();
         }
     }
 
     state->update_preset_modified();
 
-    if (timer_counter % (TIMER_HZ / UNDO_HZ) == 0 ) {
-        if (!isMouseButtonDownAnywhere()) {
+    if (timer_counter % (TIMER_HZ / UNDO_HZ) == 0)
+    {
+        if (!isMouseButtonDownAnywhere())
+        {
             processorRef.state->get_undo_manager()->beginNewTransaction();
         }
     }
