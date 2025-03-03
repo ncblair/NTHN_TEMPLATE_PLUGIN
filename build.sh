dd1250d6aee37451b44fd394531bc95c483447d3
Ncblair <nblair@berkeley.edu>
2025-03-02 20:31:49 -0500

    feat: remove ninja

--- build.sh
+++ build.sh
@@ -23,15 +23,6 @@ done
 
 ARCH=$(uname -m)
 
-# Determine the best available CMake generator.
-if [[ "$OS" == "Darwin" ]] && command -v ninja &> /dev/null; then
-    GENERATOR="-G Ninja"
-    USE_NINJA=true
-else
-    GENERATOR=""  # Use CMake's default generator.
-    USE_NINJA=false
-fi
-
 MAIN_DIR=$(pwd)
 BUILD_DIR="${MAIN_DIR}/build/${MODE}"
 
@@ -50,7 +41,6 @@ echo "PLUGIN: ${PLUGIN_NAME}"
 echo "MODE: ${MODE}"
 echo "ARCHITECTURE: ${ARCH}"
 echo "CI MODE: ${CI_MODE}"
-echo "GENERATOR: ${GENERATOR}"
 echo "CORES: ${CORES}"
 echo "================="
 
@@ -69,13 +59,13 @@ if [ ! -d "${BUILD_DIR}/CMakeFiles" ]; then
     cd "$BUILD_DIR"
     if [[ "$OS" == "Darwin" ]]; then
         if [ "$ARCH" = "x86_64" ]; then
-            cmake $GENERATOR -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_BUILD_TYPE="$MODE" -DCMAKE_CXX_FLAGS="-fcolor-diagnostics" $CI_BUILD_FLAG "$MAIN_DIR"
+            cmake -DCMAKE_OSX_ARCHITECTURES="x86_64" -DCMAKE_BUILD_TYPE="$MODE" -DCMAKE_CXX_FLAGS="-fcolor-diagnostics" $CI_BUILD_FLAG "$MAIN_DIR"
         elif [ "$ARCH" = "arm64" ]; then
-            cmake $GENERATOR -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_BUILD_TYPE="$MODE" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 $CI_BUILD_FLAG -DCMAKE_CXX_FLAGS="-fcolor-diagnostics" "$MAIN_DIR"
+            cmake -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DCMAKE_BUILD_TYPE="$MODE" -DCMAKE_OSX_DEPLOYMENT_TARGET=10.15 $CI_BUILD_FLAG -DCMAKE_CXX_FLAGS="-fcolor-diagnostics" "$MAIN_DIR"
         fi
     elif [[ "$OS" == MINGW* ]]; then
         # Windows configuration (no macOS-specific flags).
-        cmake $GENERATOR -DCMAKE_BUILD_TYPE="$MODE" $CI_BUILD_FLAG "$MAIN_DIR"
+        cmake -DCMAKE_BUILD_TYPE="$MODE" $CI_BUILD_FLAG "$MAIN_DIR"
     fi
     cd "$MAIN_DIR"
 fi
