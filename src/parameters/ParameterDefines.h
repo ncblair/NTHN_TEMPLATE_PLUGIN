55eb36cbc4f4945e4748bc1deee7c4ae32af0ca0
Ncblair <nblair@berkeley.edu>
2025-03-06 01:04:17 -0500

    autogenned files

--- src/parameters/ParameterDefines.h
+++ src/parameters/ParameterDefines.h
@@ -1,32 +1,33 @@
 #pragma once
 #include <juce_core/juce_core.h>
-enum PARAM { GAIN, TOTAL_NUMBER_PARAMETERS };
+enum PARAM {
+	GAIN,
+	TOTAL_NUMBER_PARAMETERS
+};
 static const std::array<juce::Identifier, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_IDS{
-    "GAIN",
+	"GAIN",
 };
 static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_NAMES{
-    "GAIN",
+	"GAIN",
 };
-static const std::array<juce::NormalisableRange<float>, PARAM::TOTAL_NUMBER_PARAMETERS>
-    PARAMETER_RANGES{
-        juce::NormalisableRange<float>(0.0f, 100.0f, 0.0f, 1.0f),
-    };
-static const std::array<float, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_DEFAULTS{
-    50.0f,
+static const std::array<juce::NormalisableRange<float>, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_RANGES {
+	juce::NormalisableRange<float>(0.0f, 100.0f, 0.0f, 1.0f),
 };
-static const std::array<bool, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_AUTOMATABLE{
-    true,
+static const std::array<float, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_DEFAULTS {
+	50.0f,
+};
+static const std::array<bool, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_AUTOMATABLE {
+	true,
 };
 static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_NICKNAMES{
-    "Gain",
+	"Gain",
 };
-static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_SUFFIXES{
-    "%",
+static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_SUFFIXES {
+	"%",
 };
-static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_TOOLTIPS{
-    "Loudness Parameter",
+static const std::array<juce::String, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_TOOLTIPS {
+	"Loudness Parameter",
 };
-static const std::array<std::vector<juce::String>, PARAM::TOTAL_NUMBER_PARAMETERS>
-    PARAMETER_TO_STRING_ARRS{
-        std::vector<juce::String>{},
-    };
+static const std::array<std::vector<juce::String>, PARAM::TOTAL_NUMBER_PARAMETERS> PARAMETER_TO_STRING_ARRS {
+	std::vector<juce::String>{},
+};
--- src/plugin/ProjectInfo.cpp
+++ src/plugin/ProjectInfo.cpp
@@ -3,6 +3,6 @@
 #include "ProjectInfo.h"
 
 const int ProjectInfo::versionNumber = 0x000001;
-const char *const ProjectInfo::versionString = "0.0.1";
-const char *const ProjectInfo::companyName = "YOUR_COMPANY_NAME";
-const char *const ProjectInfo::projectName = "EXAMPLE";
+const char* const ProjectInfo::versionString = "0.0.1";
+const char* const ProjectInfo::companyName = "YOUR_COMPANY_NAME";
+const char* const ProjectInfo::projectName = "EXAMPLE";
